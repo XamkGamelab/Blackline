60020aaac2439185071f12ad42a79c70278b9ebf
XamkShad <fshan001@edu.xamk.fi>
2025-11-26 18:54:40 +0200

    Animation Integration and IK

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
-
+    [SerializeField]
+    private Transform _leftHandIK;
+    [SerializeField] 
+    private Transform _rightHandIK;
 }
